d668be817181585a9a4c20d04da8ba08574f5a61
Aslam <54565458+aslamnip@users.noreply.github.com>
2023-06-01 16:18:47 +0600

    Update Form.js

--- src/app/api/apis/Form/Form.js
+++ src/app/api/apis/Form/Form.js
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import Router from `next/router`;
+import Router from 'next/router';
 
 const Form = (props) => {
     const { singleContennt } = props
